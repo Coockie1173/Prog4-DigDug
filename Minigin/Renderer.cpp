42aa7cf9a13345ece4a7865efa3b233572aa2863
.Coockie <rudjerud@hotmail.com>
2026-02-28 13:27:34 +0100

    fix imgui render bug

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -45,23 +45,21 @@ void dae::Renderer::Init(SDL_Window* window)
 
 void dae::Renderer::Render() const
 {
-	ImGui_ImplSDLRenderer3_NewFrame();
-	ImGui_ImplSDL3_NewFrame();
-	ImGui::NewFrame();
-
-	//ImGui::ShowDemoWindow(); // For demonstration purposes, do not keep this in your engine
-
-	ImGui::Render();
-
 	const auto& color = GetBackgroundColor();
 	SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, color.a);
 	SDL_RenderClear(m_renderer);
 
 	SceneManager::GetInstance().Render();
 
-	SDL_RenderPresent(m_renderer);
+	ImGui_ImplSDLRenderer3_NewFrame();
+	ImGui_ImplSDL3_NewFrame();
+	ImGui::NewFrame();
 
+	//ImGui::ShowDemoWindow();
+
+	ImGui::Render();
 	ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), m_renderer);
+
 	SDL_RenderPresent(m_renderer);
 }
 
